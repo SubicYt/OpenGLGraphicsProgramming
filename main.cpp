e9e7f60e41254c08ec773d12ec34db9eeadcea78
SubicYt <ethandenning844@gmail.com>
2026-07-25 11:10:14 -0400

    added stone textures

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 //instantiate a GLFW Window()
 #pragma comment(lib, "glfw3.lib")
@@ -67,15 +69,49 @@ int main() {
 	/*
 	Begin OpenGL Triangle rendering here:
 	*/
+
+
+	/*
+	THE FOLLOWING SPECIFIED SECTION OF CODE IS MEANT FOR LOADING TEXTURES:
+	*/
+	int width, height, nr_channels;
+	unsigned char* texture_data = stbi_load("C:/Users/Ethan Denning/OneDrive/Desktop/OpenGLGraphicsProgramming/stonetiles_003_diff.jpg", &width, &height, &nr_channels, 0);
+
+	if (!texture_data) {
+		std::cout << "failed to load texture data";
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	unsigned int stone_texture_object;
+	glGenTextures(1, &stone_texture_object);
+
+	//bind our texture to a Gl_TEXTURE_2D
+	glBindTexture(GL_TEXTURE_2D, stone_texture_object);
+	//set our parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//Fill in our GL_TEXTURE_2D with our image data
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_data);
+	//finally generate mipmaps
+	glGenerateMipmap(stone_texture_object);
+	//we dont need our texture_data anymore
+	stbi_image_free(texture_data);
+
+
 	float triangle_verticies[] = {
-		//first triangle (right three float values are rgb values)
-		-0.5f, -0.5f, 0.0f, 1.0f, 0.3f, 0.5f,
-		0.0f, 0.5f, 0.0f, 0.4f, 0.2f, 0.6f,
-		0.5f, -0.5f, 0.0f, 0.7f, 0.9f, 0.0f,
+		//first triangle 
+		//formatting of the following bytes:
+		//first 3 - position, second 3 - color, last 2 - texutre coords
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.3f, 0.5f, 1.0f, 1.0f,
+		0.0f, 0.5f, 0.0f, 0.4f, 0.2f, 0.6f, 1.0f, 0.0f,
+		0.5f, -0.5f, 0.0f, 0.7f, 0.9f, 0.0f, 0.0f, 1.0f,
 		//Second Triangle (right three float values are rgb values)
-		0.5f, -0.5f, 0.0f, 0.2f, 0.6f, 0.9f,
-		0.75f, 0.5f, 0.0f, 0.4f, 0.1f, 0.1f,
-		0.9f, -0.5f, 0.0f, 0.6f, 1.0f, 0.4f
+		0.5f, -0.5f, 0.0f, 0.2f, 0.6f, 0.9f, 1.0f, 1.0f,
+		0.75f, 0.5f, 0.0f, 0.4f, 0.1f, 0.1f, 1.0f, 0.0f,
+		0.9f, -0.5f, 0.0f, 0.6f, 1.0f, 0.4f, 0.0f, 1.0f
 	};
 	
 	unsigned int VAO; //vector array object
@@ -93,12 +129,16 @@ int main() {
 	const char* vertex_shader = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
 		"layout (location = 1) in vec3 aColor; \n"
+		"layout(location = 2) in vec2 aTexCoords; \n"
 
 		"out vec3 ourColor;\n"
+		"out vec2 texture_coordinates; \n"
+
 		"void main()\n"
 		"{\n"
 		"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
 		"ourColor = aColor;\n"
+		"texture_coordinates = aTexCoords;\n"
 		"}\0";
 
 	unsigned int triangles_shader_obj;
@@ -121,9 +161,17 @@ int main() {
 	const char* fragment_shader = "#version 330 core\n"
 		"out vec4 fragment_data;\n"
 		"in vec3 ourColor;\n"
+		"in vec2 texture_coordinates; \n"
+		"uniform sampler2D our_texture;\n"
+		/*
+		 GLSL has a built-in data-type for texture objects called
+		a sampler that takes as a postfix the texture type we want e.g. sampler1D, sampler3D or in
+		our case sampler2D. We can then add a texture to the fragment shader by simply declaring a
+		uniform sampler2D that we later assign our texture to.
+		*/
 		"void main() \n"
 		"{\n"
-		"fragment_data = vec4(ourColor, 1.0f);\n"
+		"fragment_data = texture(our_texture, texture_coordinates);\n"
 		"}\0";
 
 	unsigned int fragment_shader_object;
@@ -155,23 +203,25 @@ int main() {
 	glDeleteShader(triangles_shader_obj);
 	glDeleteShader(fragment_shader_object);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(1);
 
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	while (!glfwWindowShouldClose(window)) {
 		user_close_input(window);
 
-		glClearColor(0.7f, 0.2f, 0.3f, 1.0f);
+		glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 
 		glUseProgram(shader_program);
 		
-
+		glBindTexture(GL_TEXTURE_2D, stone_texture_object);
 		glBindVertexArray(VAO); //why do it a second time in our render loop?
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
