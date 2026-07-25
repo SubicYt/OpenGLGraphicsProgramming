8e4c2bb1230398a07b869ec2dd6ae3aed5f8f70a
SubicYt <ethandenning844@gmail.com>
2026-07-25 08:47:44 -0400

    updated colors

--- main.cpp
+++ main.cpp
@@ -68,14 +68,14 @@ int main() {
 	Begin OpenGL Triangle rendering here:
 	*/
 	float triangle_verticies[] = {
-		//first triangle
-		-0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		//Second Triangle
-		0.5f, -0.5f, 0.0f,
-		0.75f, 0.5f, 0.0f,
-		0.9f, -0.5f, 0.0f
+		//first triangle (right three float values are rgb values)
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.3f, 0.5f,
+		0.0f, 0.5f, 0.0f, 0.4f, 0.2f, 0.6f,
+		0.5f, -0.5f, 0.0f, 0.7f, 0.9f, 0.0f,
+		//Second Triangle (right three float values are rgb values)
+		0.5f, -0.5f, 0.0f, 0.2f, 0.6f, 0.9f,
+		0.75f, 0.5f, 0.0f, 0.4f, 0.1f, 0.1f,
+		0.9f, -0.5f, 0.0f, 0.6f, 1.0f, 0.4f
 	};
 	
 	unsigned int VAO; //vector array object
@@ -92,9 +92,13 @@ int main() {
 	//setup our vertex shader
 	const char* vertex_shader = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
+		"layout (location = 1) in vec3 aColor; \n"
+
+		"out vec3 ourColor;\n"
 		"void main()\n"
 		"{\n"
 		"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+		"ourColor = aColor;\n"
 		"}\0";
 
 	unsigned int triangles_shader_obj;
@@ -106,7 +110,7 @@ int main() {
 	//check for a successful shader compile
 	int vertex_shader_status;
 	glGetShaderiv(triangles_shader_obj, GL_COMPILE_STATUS, &vertex_shader_status);
-	
+
 	if (!vertex_shader_status) {
 		std::cout << "vertex shader failed to compile";
 		glfwTerminate();
@@ -116,9 +120,10 @@ int main() {
 	
 	const char* fragment_shader = "#version 330 core\n"
 		"out vec4 fragment_data;\n"
+		"in vec3 ourColor;\n"
 		"void main() \n"
 		"{\n"
-		"fragment_data = vec4(0.5f, 0.9f, 1.0f, 1.0f);\n"
+		"fragment_data = vec4(ourColor, 1.0f);\n"
 		"}\0";
 
 	unsigned int fragment_shader_object;
@@ -150,8 +155,11 @@ int main() {
 	glDeleteShader(triangles_shader_obj);
 	glDeleteShader(fragment_shader_object);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
 
 
 	while (!glfwWindowShouldClose(window)) {
@@ -160,7 +168,10 @@ int main() {
 		glClearColor(0.7f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+
 		glUseProgram(shader_program);
+		
+
 		glBindVertexArray(VAO); //why do it a second time in our render loop?
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
